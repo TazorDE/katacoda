1177f43df9c689fbf204aae77b33d3393b7f080e
Nils Malte Teichert <Nils.Teichert@ibm.com>
2022-04-11 12:21:02 +0200

    add data, rm downloading

--- Neo4j/foreground.sh
+++ Neo4j/foreground.sh
@@ -8,10 +8,3 @@ echo "Docker started"
 echo "Setting up Neo4j environment in docker..."
 docker run -d --log-driver=none --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/securepw neo4j:latest
 echo "done"
-
-# load dataset from https://www.kaggle.com/datasets/shivamb/netflix-shows
-echo "Downloading dataset from https://www.kaggle.com/datasets/shivamb/netflix-shows"
-wget https://www.kaggle.com/datasets/shivamb/netflix-shows/download
-unzip archive.zip
-rm archive.zip
-echo "done"

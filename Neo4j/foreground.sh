a4d647b6791aaea44417a453fbab36ee29a43980
Nils Malte Teichert <Nils.Teichert@ibm.com>
2022-04-11 11:31:08 +0200

    Update foreground.sh

--- Neo4j/foreground.sh
+++ Neo4j/foreground.sh
@@ -6,7 +6,7 @@ echo "Docker started"
 
 # set up Neo4j environment in docker
 echo "Setting up Neo4j environment in docker..."
-docker run --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/securepw neo4j:latest
+docker run -d --log-driver=none --name neo4j -p 7474:7474 -p 7687:7687 --env NEO4J_AUTH=neo4j/securepw neo4j:latest
 echo "done"
 
 # load dataset from https://www.kaggle.com/datasets/shivamb/netflix-shows
